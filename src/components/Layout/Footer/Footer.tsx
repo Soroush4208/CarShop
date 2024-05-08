baf40c767198d84ce9e181fef8ffca14492cae4d
soroush <soroush.r80@gmail.com>
2024-05-08 15:05:10 +0330

    feat : Create Footer web

---
+++ src/components/Layout/Footer/Footer.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Footer() {
+  return (
+    <div className="bg-[#3b82f6] flex justify-center items-center font-semibold text-white py-2 absolute bottom-0 w-full">
+      <h1>🤩 Thank You For Your Visit My Web 🤩</h1>
+    </div>
+  )
+}
+
+export default Footer
