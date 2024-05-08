d1dcda7c28cbbeffa320c9dd2b62bd2628788203
soroush <soroush.r80@gmail.com>
2024-05-08 15:03:08 +0330

    feat : Create Home Components

---
+++ src/components/Home/HomeComponent.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Layout from '../Layout/Layout'
+import Product from './Product/Product'
+
+function HomeComponent() {
+  return (
+    <Layout>
+      <Product/>
+    </Layout>
+  )
+}
+
+export default HomeComponent
