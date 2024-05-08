bf54e5fb4a25f46a57881c73def1a97bb242b2eb
soroush <soroush.r80@gmail.com>
2024-05-08 15:03:49 +0330

    feat : Create Header web

---
+++ src/components/Layout/Header/Header.tsx
@@ -0,0 +1,10 @@
+
+const Header = () => {
+  return (
+    <div className="bg-[#3b82f6] flex justify-center items-center font-semibold text-white py-2">
+      <h1>My Web Shop</h1>
+    </div>
+  )
+}
+
+export default Header
