5c1bf3e20d1525c6a9ce70498181a6fd2c0e313e
soroush <soroush.r80@gmail.com>
2024-05-08 15:34:09 +0330

    style : style button product

--- src/components/Home/Product/Product.tsx
+++ src/components/Home/Product/Product.tsx
@@ -44,13 +44,13 @@ function Product() {
                 key={product.id}
               >
                 <div className="flex flex-col gap-1">
-                <img className="w-48" src={product.image} alt={product.name} />
+                <img className="w-48 rounded-sm" src={product.image} alt={product.name} />
                 <h2 className="font-bold">{product.name}</h2>
                 <h3 className="font-medium text-sm">{`$ ${product.price} Millions`}</h3>
                 </div>
                 <div className="flex flex-col justify-between h-16">
                 <p className="text-gray-500 text-[10px]">{product.desc}</p>
-                <button className="flex justify-center items-center bg-[#3b82f6] w-full py-1 text-white">
+                <button className="flex justify-center items-center bg-[#3b82f6] w-full py-1 text-white rounded-sm">
                   {product.condition}
                 </button>
                 </div>
