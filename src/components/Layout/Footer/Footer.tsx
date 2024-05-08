708628a3ec32abf88c451d4612b1b29d2b555600
soroush <soroush.r80@gmail.com>
2024-05-08 15:27:35 +0330

    ref : refactor code

--- src/components/Layout/Footer/Footer.tsx
+++ src/components/Layout/Footer/Footer.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 function Footer() {
   return (
     <div className="bg-[#3b82f6] flex justify-center items-center font-semibold text-white py-2 absolute bottom-0 w-full">
-      <h1>🤩 Thank You For Your Visit My Web 🤩</h1>
+      <h1>🤩 Thank You For Visit My Web 🤩</h1>
     </div>
   )
 }
